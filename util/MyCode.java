93d15098d5b1a782f7ebbbaaded5fc02860b2c2e
Agneet Chatterjee <agneet257@gmail.com>
2017-07-23 17:41:29 +0530

    updated

--- util/MyCode.java
+++ util/MyCode.java
@@ -11,6 +11,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.TreeMap;
 import java.util.PriorityQueue;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
 
 import wsc08composition.bean.Concept;
 import wsc08composition.bean.EdgeVariable;
@@ -23,10 +33,11 @@ import wsc08composition.bean.VertextVariable;
 
 public class MyCode{
     private ArrayList<Service> ServiceList = new ArrayList<Service>();  // ArrayList to store all services
-    private ArrayList<ArrayList<Instance>> inputInstances = new ArrayList<ArrayList<Instance>>(); // ArrayList of all ArrayLists to store input instances of each Service
+    private ArrayList<ArrayList<Instance>> inputInstances = new ArrayList<ArrayList<Instance>>();// ArrayList of all ArrayLists to store input instances of each Service
     public void getServices1(){
         ReadFile rf = new ReadFile();
-        rf.readServiceFile("/home/agneet42/Documents/ISI/satsolver101/WSC08Composition/Testset01/services.xml"); // calling readServiceFile
+        rf.readConceptFile("Testset05ma/taxonomy.xml");
+        rf.readServiceFile("Testset05/services.xml"); // calling readServiceFile
         ServiceList = rf.getServices(); // storing all services of TestSet01 in ServiceList
         int f = 0;
         for(Service iterService : ServiceList){ // outer loop
@@ -35,33 +46,41 @@ public class MyCode{
             OutputService.add(iterService);
             for(Service iterService1 : ServiceList){ // inner loop
                 if(iterService != iterService1){ // checking for equality. if equal, ignoring
-                   inputInstances.add(iterService1.getInput()); // getting inputinstance of iterService1
-                   for(Instance temp : inputInstances.get(0)){  // iterating over 0-index of inputInstance which is the list of all inputInstance of iterService
+                   inputInstances.add(iterService1.getInput()); // getting inputinstance 
+                   for(Instance temp : inputInstances.get(0)){ // iterating over 0-index of inputInstance which is the list of all inputInstance of iterService
                        for(Instance temp1 : inputInstances.get(1)){ // iterating over 1-index of inputInstance which is the list of all inputInstance of iterService
                            boolean check = Check(temp,temp1); // calling function Check to check for concepts and superconcepts
-                           if(check==false){
-                               f=1;
-                               break;
+                           if(check==true){
+                               f=f+1;
+                                }
                             }
                         }
-                        if(f==1){
-                            f=0;break;
-                        }
                     }
-                }
-                if(f==0){
+                else
+                    break;
+                // System.out.print(inputInstances.get(0).get(0).getName());
+                 if(f >= inputInstances.get(1).size()){
                     OutputService.add(iterService1);
+                    f=0;
                 }
+                inputInstances.remove(1);
             }
             if(OutputService.size()!=1){
+                System.out.print(OutputService.get(0).getName() + " : ");
                 for(Service print1 : OutputService){
-                    System.out.println(print1);       //the only change needed in the code to print the required attribute of the Service
-                }
+                    System.out.print(print1.getName()+" ");       //the only change needed in the code to print the required attribute of the Service
+                } 
+                f=0;
+                System.out.println();
             }
+            inputInstances.remove(0);
+            
+          
+            
         }
-}
-
-    public boolean Check(Instance i1, Instance i2){
+    }
+  
+public boolean Check(Instance i1, Instance i2){
      Concept a = i1.getConcept();
      while (true){
             if (a == null)break;
@@ -71,11 +90,13 @@ public class MyCode{
         }
      return false;
     }
+    public static void main(String args[])
+    {
+        MyCode obj = new MyCode();
+        obj.getServices1();
+    }
 }
         
         
       
-
-
-
 
